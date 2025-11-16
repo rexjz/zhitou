6a53764bb8f61a747986765b422b3fa2ecd56452
xujingzhou <max3.xu@outlook.com>
2025-11-17 02:51:07 +0800

    feat: annual_report_worker

--- packages/worker/src/worker/annual_report_worker/__init__.py
+++ packages/worker/src/worker/annual_report_worker/__init__.py
@@ -1,5 +1,9 @@
-from ragflow_sdk import RAGFlow
 from worker.config import WorkerConfigLoader, WorkerConfig
+from urllib.parse import urljoin
+from ragflow_sdk import RAGFlow
+import urllib.request
+import urllib.error
+import json
 
 
 def main() -> None:
@@ -9,3 +13,19 @@ def main() -> None:
   print("Hello from worker!")
 
   rag_object = RAGFlow(api_key=config.ragflow.apikey, base_url=config.ragflow.url)
+  print(rag_object.api_url)
+
+  print(rag_object.list_datasets())
+  
+  # Make GET request to healthz endpoint using urllib
+  url = urljoin(config.ragflow.url , "/v1/system/healthz")
+  print(url)
+  try:
+    with urllib.request.urlopen(url) as response:
+      data = json.loads(response.read().decode())
+      print(data)
+  except urllib.error.HTTPError as e:
+    # Print response even on HTTP errors (non-200 status codes)
+    data = json.loads(e.read().decode())
+    print(f"HTTP Error {e.code}: {data}")
+  
