77df73177ebda984c37530df16a95907980d1f66
xujingzhou <max3.xu@outlook.com>
2025-12-29 04:49:18 +0800

    feat: agui proxy endpoint

--- packages/zhitou_agent/src/zhitou_agent/ag_ui/agui_app.py
+++ packages/zhitou_agent/src/zhitou_agent/ag_ui/agui_app.py
@@ -1,12 +1,17 @@
 from agno.os import AgentOS
 from agno.os.interfaces.agui import AGUI
 from zhitou_agent.agent.agno import create_agno_zhitou_agent
+from zhitou_agent.config import ZhitouAgentConfigLoader
 
 
 def create_agui_agno_app(session_id, user_id):
+  config = ZhitouAgentConfigLoader().load()
+
   agent = create_agno_zhitou_agent(
     session_id=session_id,
     user_id=user_id,
+    bocha=config.bocha,
+    dashscpope=config.dashscpope
   )
   agui_interface = AGUI(agent=agent)
   agent_os = AgentOS(agents=[agent], interfaces=[agui_interface])
